4904ac0a64df911489020cba6c2c82ae4cd2010a
wangrb5 <wangrb5@yonyou.com>
2017-08-22 18:45:23 +0800

    todo
    Change-Id: Ie1bd3512ae47e57318e6084df05dc4fa2487e1ff
    Signed-off-by: wangrb5 <wangrb5@yonyou.com>

---
+++ src/com/lichking/solution/LinkedListMerge.java
@@ -0,0 +1,12 @@
+package com.lichking.solution;
+
+import com.lichking.entity.ListNode;
+
+public class LinkedListMerge {
+
+	public ListNode Merge(ListNode list1, ListNode list2) {
+		return null;
+	}
+
+}
+ 
