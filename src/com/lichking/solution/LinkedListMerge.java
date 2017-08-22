483b3255cb651a6280afcc669783649ffbc8f8ec
LichKing <lichking_coder@foxmail.com>
2017-08-22 20:56:14 +0800

    单调递增链表合并
    Change-Id: Ia5fed1f6b6aa33f69ad6b26b3f0ce6f1c526f456
    Signed-off-by: LichKing <lichking_coder@foxmail.com>

--- src/com/lichking/solution/LinkedListMerge.java
+++ src/com/lichking/solution/LinkedListMerge.java
@@ -2,10 +2,50 @@ package com.lichking.solution;
 
 import com.lichking.entity.ListNode;
 
+/**
+ * 输入两个单调递增的链表，输出两个链表合成后的链表，
+ * 当然我们需要合成后的链表满足单调不减规则。
+ * @author LichKing
+ *
+ */
 public class LinkedListMerge {
 
 	public ListNode Merge(ListNode list1, ListNode list2) {
-		return null;
+		
+		if(list1 != null && list2 == null)
+			return list1;
+		else if(list1 == null && list2 != null)
+			return list2;
+		else if(list1 == null && list2 == null)
+			return null;
+		ListNode head,cursor;
+		if(list1.val < list2.val){
+			cursor = head = list1;
+			list1 = list1.next;
+		}else{
+			cursor = head = list2;
+			list2 = list2.next;
+		}
+			
+		while(list1 != null || list2 != null){
+			if(list1 != null && list2 == null){
+				cursor.next = list1;
+				break;
+			}else if(list2 != null  && list1 == null){
+				cursor.next = list2;
+				break;
+			}else{
+				if(list1.val < list2.val){
+					cursor.next = list1;
+					list1 = list1.next;
+				}else{
+					cursor.next = list2;
+					list2 = list2.next;
+				}
+				cursor = cursor.next;
+			}
+		}
+		return head;
 	}
 
 }
